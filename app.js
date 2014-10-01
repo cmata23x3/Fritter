28dc1dfc26a0f1fecac3d0cd3a8507078b5a0e22
Mata <cmata@mit.edu>
2014-10-01 01:56:47 -0400

    Have home page made

--- app.js
+++ app.js
@@ -3,26 +3,22 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
+var session = require('express-session');
+var flash = require('connect-flash');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var User = require('./data/models/users.js');
 
 var app = express();
 
 var dbURL = 'mongodb://localhost/fritter';
-
 if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-  dbURL = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ':' +
-          process.env.OPENSHIFT_MONGODB_DB_PASSWORD + '@' +
-          process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-          process.env.OPENSHIFT_MONGODB_DB_PORT + '/fritter';
+	dbURL = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ':' +
+	process.env.OPENSHIFT_MONGODB_DB_PASSWORD + '@' +
+	process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+	process.env.OPENSHIFT_MONGODB_DB_PORT + '/fritter';
 }
-
 var mongoose = require('mongoose').connect(dbURL);
-
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Mongoose connection error'));
 
@@ -35,56 +31,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser('secret'));
 app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use(express.session({
-//   secret: 'secret',
-//   maxAge: 3600000
-// }));
-
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(session({ resave: true,
+	saveUninitialized: true,
+	secret: '$ecRe7' }));
+app.use(flash());
 
 app.use('/', routes);
 app.use('/users', users);
 
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
- 
-passport.deserializeUser(function(user, done) {
-  User.findById(id, function(err, user) {
-    done(err, user);
-  });
-  done(null, user);
-});
-
-passport.use(new LocalStrategy(function(username, password, done) {
-  process.nextTick(function() {
-    User.findOne({
-      'username': username, 
-    }, function(err, user) {
-      if (err) {
-        return done(err);
-      }
- 
-      if (!user) {
-        return done(null, false);
-      }
- 
-      if (user.password != password) {
-        return done(null, false);
-      }
- 
-      return done(null, user);
-    });
-  });
-}));
+app.use(function(req, res, next){
+	res.locals.session = req.session;
+	next();
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handlers
@@ -92,27 +56,27 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
 });
 
 console.log('Its running on: ', (process.env.OPENSHIFT_NODEJS_PORT || 8080));
 app.listen(process.env.OPENSHIFT_NODEJS_PORT || 8080,
-           process.env.OPENSHIFT_NODEJS_IP);
+	process.env.OPENSHIFT_NODEJS_IP);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var passport = require('passport');
+var User = require('../data/models/users.js');
+var Tweets = require('../data/models/tweets.js');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -9,22 +10,51 @@ router.get('/', function(req, res) {
 
 /* GET login page*/
 router.get('/login', function(req, res){
-    res.render('login', {title: 'Log In'});
+	res.render('login', {title: 'Log In'});
 });
 
-router.post('/login',
-  passport.authenticate('local', {
-    successRedirect: '/loginSuccess',
-    failureRedirect: '/loginFailure'
+/*POST login page */
+router.post('/login', function(req, res){
+	var username = req.body.username;
+	var pwrd = req.body.password; 
+	User.findOne({'username': username, }, function(err, user){
+    	if(user){//found a user
+    		if(pwrd !== user.password ){
+    			console.log("passwords don't match");
+    			req.flash('info', 'Password does not match User Name');
+    			res.redirect('/login');
+    		}else{//we're good; make name
+    			req.session.user = username;
+    			res.redirect('/home');
+    		}
+    	}
+    	else{//no username found!
+    		console.log("User name doesn't exist!");
+    		req.flash('info', "User name doesn't exist!")
+    		res.redirect('/login');
+    	}
   })
-);
- 
-router.get('/loginFailure', function(req, res, next) {
-  res.send('Failed to authenticate');
 });
- 
-router.get('/loginSuccess', function(req, res, next) {
-  res.send('Successfully authenticated');
+
+router.delete('/sessions', function(req, res){
+	req.session.destroy();
+	res.redirect('/');
+});
+
+router.get('/home', function(req, res) {
+    // var collection = req.db.collection('usercollection');
+    Tweets.find({}, function(err, docs){
+        if(err){
+            res.json(err);
+        }
+        else{
+            res.render('home', {
+            "title": "Home",
+            "name": req.session.user,
+            "tweets": docs
+            });
+        }
+    });
 });
 
 module.exports = router;
