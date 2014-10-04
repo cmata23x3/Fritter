007963bcdbf61122761de700d2b3bee4ea29f693
Mata <cmata@mit.edu>
2014-10-03 21:58:15 -0400

    Added form toggling & some styling

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,13 @@
 //This is the file to use jquery
 
+/**
+* 
+*
+*/
 $(".editButton").click(function(){
 	console.log("clicked on edit!");
+	var parent = $(this).parent();
+	$(this).toggle();
+	parent.find($(".deleteForm")).toggle();
+	parent.find($(".editForm") ).toggle();
 });
